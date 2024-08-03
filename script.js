9cf0deae91ece83b60f7c41a784b841dbbfeb4e9
Folarin Samuel <167587177+folarin-samuel@users.noreply.github.com>
2024-08-03 08:19:12 +0100

    Update script.js
    Signed-off-by: Folarin Samuel  <167587177+folarin-samuel@users.noreply.github.com>

--- script.js
+++ script.js
@@ -11,32 +11,32 @@ btn3.addEventListener('click', () => {location.href="third.html"});
 btn4.addEventListener('click', () => {location.href="fourth.html"});
 btn5.addEventListener('click', () => {location.href="fifth.html"});
 function pre(){
-				if(location.href=="file:///storage/emulated/0/webpages/index.html"){
+				if(location.href=="https://folarin-samuel.github.io/index.html"){
 								alert("This is the first page");
-				} else if(location.href=="file:///storage/emulated/0/webpages/second.html"){
-								location.href="index.html";
-				} else if(location.href=="file:///storage/emulated/0/webpages/third.html"){
-								location.href="second.html";
-				} else if(location.href=="file:///storage/emulated/0/webpages/fourth.html"){
-								location.href="third.html";
-				} else if(location.href=="file:///storage/emulated/0/webpages/fifth.html"){
-								location.href="fourth.html";
+				} else if(location.href=="https://folarin-samuel.github.io/second.html"){
+								location.href="https://folarin-samuel.github.io/index.html";
+				} else if(location.href=="https://folarin-samuel.github.io/third.html"){
+								location.href="https://folarin-samuel.github.io/second.html";
+				} else if(location.href=="https://folarin-samuel.github.io/fourth.html"){
+								location.href="https://folarin-samuel.github.io/third.html";
+				} else if(location.href=="https://folarin-samuel.github.io/fifth.html"){
+								location.href="https://folarin-samuel.github.io/fourth.html";
 				}
 				
 }
 function nexts(){
-				if(location.href=="file:///storage/emulated/0/webpages/index.html"){
-								location.href="second.html"
-				} else if(location.href=="file:///storage/emulated/0/webpages/second.html"){
-								location.href="third.html";
-				} else if(location.href=="file:///storage/emulated/0/webpages/third.html"){
-								location.href="fourth.html";
-				} else if(location.href=="file:///storage/emulated/0/webpages/fourth.html"){
-								location.href="fifth.html";
-				} else if(location.href=="file:///storage/emulated/0/webpages/fifth.html"){
+				if(location.href=="https://folarin-samuel.github.io/index.html"){
+								location.href="https://folarin-samuel.github.io/second.html"
+				} else if(location.href=="https://folarin-samuel.github.io/second.html"){
+								location.href="https://folarin-samuel.github.io/third.html";
+				} else if(location.href=="https://folarin-samuel.github.io/third.html"){
+								location.href="https://folarin-samuel.github.io/fourth.html";
+				} else if(location.href=="https://folarin-samuel.github.io/fourth.html"){
+								location.href="https://folarin-samuel.github.io/fifth.html";
+				} else if(location.href=="https://folarin-samuel.github.io/fifth.html"){
 								alert('This is the end of the page');
 				}
 }
 prev.addEventListener('click', pre);
 next.addEventListener('click', nexts);
-alert(location.href);
+//alert(location.href)
